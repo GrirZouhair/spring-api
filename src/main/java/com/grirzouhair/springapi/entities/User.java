b0cecc7e8516c9d052fef2e198e3da371ee9744f
Grir_Zouhiar <zouhairgrir937@gmail.com>
2025-06-13 21:23:48 +0100

    Data Transfer Object (DTO)

---
+++ src/main/java/com/grirzouhair/springapi/entities/User.java
@@ -0,0 +1,28 @@
+package com.grirzouhair.springapi.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
---
+++ src/main/java/com/grirzouhair/springapi/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.grirzouhair.springapi.repositories;
+
+import com.grirzouhair.springapi.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
