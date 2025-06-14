8c27927a6ffef93888333e81bf83b53eb3211b2d
Grir_Zouhiar <zouhairgrir937@gmail.com>
2025-06-14 11:16:40 +0100

    customizing response data (Serialization)

--- src/main/java/com/grirzouhair/springapi/mappers/UserMapper.java
+++ src/main/java/com/grirzouhair/springapi/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.grirzouhair.springapi.mappers;
 import com.grirzouhair.springapi.dtos.UserDto;
 import com.grirzouhair.springapi.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /*
 componentModel = "spring" tells MapStruct to generate
@@ -10,5 +11,6 @@ the implementation as a Spring Bean.
  */
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
